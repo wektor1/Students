dc7d4c89f8b1ebede9d623a8d6d1c18cea7ca0c3
Andrzej <ganczarek.a@gmail.com>
2018-07-13 15:19:57 +0200

    Formatting code to show Menu

--- showMenu.cpp
+++ showMenu.cpp
@@ -1,14 +1,14 @@
 #include "showMenu.hpp"
 #include <iostream>
-//#include <stdlib.h>
 
 int showMenu() {
   int t = -1;
   do {
-    //		system("clear");
-    std::cout << "1 - Dodaj studenta  \n2 - Usun studenta  \n3 - Wyswietl "
-                 "wszystkich studentow \n0 - Wyjscie z programu \nCo chcesz "
-                 "robic: ";
+    std::cout << "1 - Dodaj studenta  \n" <<
+                 "2 - Usun studenta  \n" <<
+                 "3 - Wyswietl wszystkich studentow \n" <<
+                 "0 - Wyjscie z programu \n" <<
+                 "Co chcesz robic: ";
     std::cin >> t;
   } while (t < 0 || t > 3);
   return t;
