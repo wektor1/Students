8274548e425eb82eee958a10377a3de2c8365a17
Andrzej <ganczarek.a@gmail.com>
2018-07-13 13:23:41 +0200

    Added function to clear screen

--- showMenu.cpp
+++ showMenu.cpp
@@ -1,11 +1,13 @@
 #include"showMenu.hpp"
 #include<iostream>
+#include <stdlib.h>
 
 int showMenu() {
         int t = -1;
         do {
-	std::cout << "1 - Dodaj studenta  \n2 - Usun studenta  \n3 - Wyswietl wszystkich studentow \n0 - Wyjscie z programu \nCo chcesz robic: ";
-        std::cin >> t;
+		system("clear");
+		std::cout << "1 - Dodaj studenta  \n2 - Usun studenta  \n3 - Wyswietl wszystkich studentow \n0 - Wyjscie z programu \nCo chcesz robic: ";
+      		std::cin >> t;
         } while(t<0 || t>3);
         return t;
 }
