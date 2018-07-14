ce8a2197210e9ce50db385189fc217b4404e4b80
Andrzej <ganczarek.a@gmil.com>
2018-07-14 06:31:36 +0200

    Added message when database is empty

--- showStudents.cpp
+++ showStudents.cpp
@@ -1,6 +1,10 @@
 #include "showStudents.hpp"
 
 void showStudents(std::map<int, Student> &mapStudents) {
+  if(mapStudents.empty()) {
+    std::cout << "Baza studentow jest pusta" << std::endl;
+    return;
+  }
   for (std::map<int, Student>::iterator itr = mapStudents.begin(); itr != mapStudents.end(); itr++) {
     std::cout << (itr->second).getIndex() << " \t\t\t\t "
               << (itr->second).getName() << " \t\t\t\t "
