ab67e63ecdc400d664b72822a83a7c5736c2af29
wektor1 <wektor1@gmail.com>
2018-07-15 01:23:33 +0200

    Added columns width=15

--- showStudents.cpp
+++ showStudents.cpp
@@ -1,10 +1,11 @@
 #include "showStudents.hpp"
+#include <iomanip>
 
 void showStudents(std::map<int, Student> &mapStudents) {
   for (std::map<int, Student>::iterator itr = mapStudents.begin();
        itr != mapStudents.end(); itr++) {
-    std::cout << (itr->second).getIndex() << " \t\t\t\t "
-              << (itr->second).getName() << " \t\t\t\t "
+    std::cout << std::left << std::setw(15) << (itr->second).getIndex()
+              << std::setw(15) << (itr->second).getName() << std::setw(15)
               << (itr->second).getSurname() << std::endl;
   }
   std::cin.ignore();
