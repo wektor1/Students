ffe6301d90c89317807bba6144445fdd88c79c69
wektor1 <wektor1@gmail.com>
2018-07-15 02:27:19 +0200

    Added check in addStudent if inserted index is a number

--- addStudent.cpp
+++ addStudent.cpp
@@ -12,13 +12,20 @@ void addStudent(std::map<int, Student> &map_student) {
   std::getline(std::cin, surname);
   std::cout << "Podaj index: ";
   std::cin >> index;
+  while (!std::cin) {
+    std::cin.clear();
+    std::cin.sync();
+    std::cin.ignore(15,'\n');
+    std::cout << "\nPodany index nie jest liczbą!\nPodaj index: ";
+    std::cin >> index;
+  }
 
   std::pair<std::map<int, Student>::iterator, bool> inserted;
   inserted = map_student.insert(
       std::pair<int, Student>(index, Student(index, surname, name)));
   if (inserted.second == false) {
     std::cout << "Student z podanym indexem już istnieje" << std::endl;
-    std::cin.ignore();
+    std::cin.ignore(15,'\n');
     std::cin.get();
   }
 }
