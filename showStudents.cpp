71fc50eaddb3acf56b2c72427c6c78620737bf10
Andrzej <ganczarek.a@gmil.com>
2018-07-16 17:42:46 +0200

    Change name mapStudents to map_students in showStudents()

--- showStudents.cpp
+++ showStudents.cpp
@@ -1,8 +1,8 @@
 #include "showStudents.hpp"
 #include <iomanip>
 
-void showStudents(std::map<int, Student> &mapStudents) {
-  if(mapStudents.empty()) {
+void showStudents(std::map<int, Student> &map_students) {
+  if(map_students.empty()) {
     std::cout << "Baza studentow jest pusta" << std::endl;
     return;
   }
@@ -12,7 +12,7 @@ void showStudents(std::map<int, Student> &mapStudents) {
   std::cout.fill('=');
   std::cout << std::setw(60) << "=" << std::endl;
   std::cout.fill(' ');
-  for (std::map<int, Student>::iterator itr = mapStudents.begin(); itr != mapStudents.end(); itr++) {
+  for (std::map<int, Student>::iterator itr = map_students.begin(); itr != map_students.end(); itr++) {
     std::cout << std::left << std::setw(20) << (itr->second).getIndex()
               << std::setw(20) << (itr->second).getName() 
               << std::setw(20) << (itr->second).getSurname() << std::endl;
