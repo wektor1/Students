11a04ab5f93103415225b3045cb3d19183cd005e
Andrzej <ganczarek.a@gmail.com>
2018-07-16 09:17:10 +0200

    Added Numer Indeksu, Imie, Nazwisko and ===== to showStudents()

--- showStudents.cpp
+++ showStudents.cpp
@@ -6,9 +6,15 @@ void showStudents(std::map<int, Student> &mapStudents) {
     std::cout << "Baza studentow jest pusta" << std::endl;
     return;
   }
+  std::cout << std::left << std::setw(20) << "Numer indeksu"
+            << std::setw(20) << "Imie" 
+            << std::setw(20) << "Nazwisko" << std::endl;
+  std::cout.fill('=');
+  std::cout << std::setw(60) << "=" << std::endl;
+  std::cout.fill(' ');
   for (std::map<int, Student>::iterator itr = mapStudents.begin(); itr != mapStudents.end(); itr++) {
-    std::cout << std::left << std::setw(15) << (itr->second).getIndex()
-              << std::setw(15) << (itr->second).getName() << std::setw(15)
-              << (itr->second).getSurname() << std::endl;
+    std::cout << std::left << std::setw(20) << (itr->second).getIndex()
+              << std::setw(20) << (itr->second).getName() 
+              << std::setw(20) << (itr->second).getSurname() << std::endl;
   }
 }
